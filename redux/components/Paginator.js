f0a8d55808ec7ff58fe72420f1600d364361ffa9
Neraste <neraste.herr10@gmail.com>
2017-04-02 18:21:40 +0200

    Re add paginator

---
+++ redux/components/Paginator.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { browserHistory, Link } from 'react-router'
+
+export default class Paginator extends Component {
+    render() {
+        const { location, current, last } = this.props
+        const hasNext = current != last
+        const hasPrevious = current != 1
+        const pathname = location.pathname
+
+        return (
+            <nav className="paginator-controls controls">
+                <PaginatorLink
+                    to={{pathname, query: {...location.query, page: 1}}}
+                    disabled={!hasPrevious}
+                >
+                    <i className="fa fa-angle-double-left"></i>
+                </PaginatorLink>
+                <PaginatorLink
+                    to={{pathname, query: {...location.query, page: current - 1}}}
+                    disabled={!hasPrevious}
+                >
+                    <i className="fa fa-angle-left"></i>
+                </PaginatorLink>
+                <PaginatorLink
+                    to={{pathname, query: {...location.query, page: current + 1}}}
+                    disabled={!hasNext}
+                >
+                    <i className="fa fa-angle-right"></i>
+                </PaginatorLink>
+                <PaginatorLink
+                    to={{pathname, query: {...location.query, page: last}}}
+                    disabled={!hasNext}
+                >
+                    <i className="fa fa-angle-double-right"></i>
+                </PaginatorLink>
+            </nav>
+        );
+    }
+}
+
+class PaginatorLink extends Component {
+    render () {
+        const { to, disabled } = this.props
+        if (disabled) {
+            return (
+                <div className={"control primary disabled"}>{this.props.children}</div>
+            )
+        }
+
+        return (
+            <Link to={to} className={"control primary"}>{this.props.children}</Link>
+        )
+    }
+}
--- redux/containers/LibraryPage.js
+++ redux/containers/LibraryPage.js
@@ -2,6 +2,155 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { browserHistory, Link } from 'react-router'
 import { loadWorkTypes } from '../actions'
+import Paginator from '../components/Paginator'
+
+class LibraryPage extends Component {
+    componentWillMount() {
+        this.props.loadWorkTypes()
+    }
+
+    getLibraryName = () => {
+        const libraryComponentName = this.props.children.type.WrappedComponent.name
+        switch (libraryComponentName) {
+            case 'LibraryListSong':
+                return "song"
+
+            case 'LibraryListArtist':
+                return "artist"
+
+            case 'LibraryListWork':
+                const workTypeQueryName = this.props.children.props.params.workType
+                const workTypes = this.props.workTypes
+                const workType = workTypes.find(
+                    (workType) => workType.query_name == workTypeQueryName
+                )
+
+                if (!workType) {
+                    return ""
+                }
+
+                return workType.name.toLowerCase()
+        }
+    }
+
+    getLibraryPlaceholder = (libraryName) => {
+        if (!libraryName) {
+            return ""
+        }
+
+        switch (libraryName) {
+            case "song":
+                return "What will you sing?"
+
+            case "artist":
+                return "Who are you looking for?"
+
+            default:
+                return `What ${libraryName} do you want`
+        }
+    }
+
+    render() {
+        // you MUST provide the current `pathname`, otherwize (when providing
+        // only `query`) it is undefined
+        const pathname = this.props.location.pathname
+
+        // info bar
+        const entriesCount = this.props.entriesCount
+
+        // search bar
+        const queryFromUrl = this.props.location.query.search
+        let query
+
+        // library name
+        const libraryName = this.getLibraryName()
+        const libraryPlaceholder = this.getLibraryPlaceholder(libraryName)
+
+        // Work Types links
+        const workTypesTabs = this.props.workTypes.map(function(workType) {
+                    return (
+                            <LibraryTab
+                                queryName={workType.query_name}
+                                iconName={workType.icon_name}
+                                name={workType.name + "s"}
+                            />
+                           )
+        })
+
+        // counter
+        let infoCounter
+        if (libraryName) {
+            infoCounter = (
+                <div className="info-item" id="library-amount">
+                    <span className="stat">{entriesCount}</span>
+                    <span className="description">{libraryName}{entriesCount == 1? '': 's'} found</span>
+                </div>
+            )
+        }
+
+        return (
+            <div id="libraries">
+                <nav id="library-chooser">
+                    <LibraryTab
+                        queryName="song"
+                        iconName="home"
+                        extraClassName="library-tab-home"
+                    />
+                    <LibraryTab
+                        queryName="artist"
+                        iconName="music"
+                        name="Artists"
+                    />
+                    {workTypesTabs}
+                </nav>
+
+                <form id="library-searchbox" onSubmit={e => {
+                    e.preventDefault()
+                    browserHistory.push({pathname, query: {search: query.value}})
+                }}>
+                    <div className="field">
+                        <div className="fake-input">
+                            <input
+                                ref={node => {
+                                    query = node
+                                }}
+                                defaultValue={queryFromUrl || ''}
+                                placeholder={libraryPlaceholder}
+                            />
+                            <div className="controls">
+                                <div className="clear control" onClick={e => {
+                                        query.value = ""
+                                        browserHistory.push({pathname})
+                                    }
+                                }>
+                                    <i className="fa fa-times"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="controls">
+                        <button type="submit" className="search control primary">
+                            <i className="fa fa-search"></i>
+                        </button>
+                    </div>
+                </form>
+
+                {this.props.children}
+
+                <div id="paginator">
+                    <Paginator
+                        location={this.props.location}
+                        current={this.props.currentPageNumber}
+                        last={this.props.lastPageNumber}
+                    />
+                    <div className="info">
+                        {infoCounter}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
 class LibraryTab extends Component {
     render() {
@@ -26,100 +175,6 @@ class LibraryTab extends Component {
                     {tabName}
                 </Link>
                 )
-    }
-}
-
-class LibraryPage extends Component {
-    componentWillMount() {
-        this.props.loadWorkTypes()
-    }
-
-    render() {
-
-        // you MUST provide the current `pathname`, otherwize (when providing
-        // only `query`) it is undefined
-        const pathname = this.props.location.pathname
-
-        // paginator
-        const currentPageNumber = this.props.currentPageNumber
-        const lastPageNumber = this.props.lastPageNumber
-        const entriesCount = this.props.entriesCount
-
-        // search bar
-        const queryFromUrl = this.props.location.query.search
-        let query
-
-        // Work Types links
-        const workTypesTabs = this.props.workTypes.map(function(workType) {
-                    return (
-                            <LibraryTab
-                                queryName={workType.query_name}
-                                iconName={workType.icon_name}
-                                name={workType.name + "s"}
-                            />
-                           )
-        })
-
-        return (
-            <div id="libraries">
-                <nav id="library-chooser">
-                    <LibraryTab
-                        queryName="song"
-                        iconName="home"
-                        extraClassName="library-tab-home"
-                    />
-                    <LibraryTab
-                        queryName="artist"
-                        iconName="music"
-                        name="Artists"
-                    />
-                    {workTypesTabs}
-                </nav>
-
-
-
-                <form id="library-searchbox" onSubmit={e => {
-                    e.preventDefault()
-                    browserHistory.push({pathname, query: {search: query.value}})
-                }}>
-                    <div className="field">
-                        <div className="fake-input">
-                            <input
-                                ref={node => {
-                                    query = node
-                                }}
-                                defaultValue={queryFromUrl || ''}
-                                placeholder="Type something here..."
-                            />
-                            <div className="controls">
-                                <div className="clear control" onClick={e => {
-                                        query.value = ""
-                                        browserHistory.push({pathname})
-                                    }
-                                }>
-                                    <i className="fa fa-times"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="controls">
-                        <button type="submit" className="search control primary">
-                            <i className="fa fa-search"></i>
-                        </button>
-                    </div>
-                </form>
-
-                {this.props.children}
-
-                <nav>
-                    <Link to={{pathname, query: {...this.props.location.query, page: 1}}}>First</Link>
-                    <Link to={{pathname, query: {...this.props.location.query, page: currentPageNumber - 1}}}>Previous</Link>
-                    <Link to={{pathname, query: {...this.props.location.query, page: currentPageNumber + 1}}}>Next</Link>
-                    <Link to={{pathname, query: {...this.props.location.query, page: lastPageNumber}}}>Last</Link>
-                    {entriesCount} entries
-                </nav>
-            </div>
-        )
     }
 }
 
