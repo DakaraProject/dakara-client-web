fcbe645153919f9386e744189c391c0bc84ec133
Neraste <neraste.herr10@gmail.com>
2017-07-30 22:46:11 +0200

    Remove user at logout

--- src/jsx/reducers/users.js
+++ src/jsx/reducers/users.js
@@ -1,4 +1,5 @@
 import { CURRENT_USER_REQUEST, CURRENT_USER_SUCCESS, CURRENT_USER_FAILURE } from '../actions'
+import { LOGOUT } from '../actions'
 
 
 /**
@@ -9,6 +10,9 @@ function users(state = null, action) {
     switch (action.type) {
         case CURRENT_USER_SUCCESS:
             return action.response
+
+        case LOGOUT:
+            return null
 
         default:
             return state
