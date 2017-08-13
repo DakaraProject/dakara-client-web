eed4e335d018a7ca3d9c4f7c99ef48d1d9fcea84
Nadeflore <flore.love@gmail.com>
2017-08-13 19:50:46 +0200

    Disable play skip buttons when not allowed
    Add check null object on permission class playlist manager or owner
    Only allow one child for permission classes, avoid wrapping

--- src/jsx/components/Player.js
+++ src/jsx/components/Player.js
@@ -3,6 +3,7 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import utils from '../utils'
 import SongPreviewDetails from './SongPreviewDetails'
 import UserWidget from '../containers/UserWidget'
+import { IsPlaylistManagerOrOwner } from '../containers/PlaylistPermissions'
 
 export default class Player extends Component {
     pollPlayerStatus = () => {
@@ -158,68 +159,76 @@ export default class Player extends Component {
             <div id="player">
                 <div className="display">
                     <div className="controls">
-                        <button
-                            className={
-                                "control primary"
-                                    + (controlDisabled ? " disabled" : "")
-                                    + (pauseError ? " managed_error" : "")
-                            }
-                            onClick={() => {
-                                this.props.sendPlayerCommands({pause: !playerCommand.pause})
-                            }
-                            }
-                            disabled={controlDisabled}
+                        <IsPlaylistManagerOrOwner
+                            object={playerStatus.playlist_entry}
+                            disable
                         >
-                            <ReactCSSTransitionGroup
-                                transitionName="managed"
-                                transitionEnterTimeout={150}
-                                transitionLeaveTimeout={150}
+                            <button
+                                className={
+                                    "control primary"
+                                        + (pauseError ? " managed_error" : "")
+                                }
+                                onClick={() => {
+                                    this.props.sendPlayerCommands({pause: !playerCommand.pause})
+                                }
+                                }
+                                disabled={controlDisabled}
                             >
-                                {playPausebtn}
-                            </ReactCSSTransitionGroup>
-                        </button>
-                        <button
-                            className={
-                                "control primary"
-                                    + (controlDisabled ? " disabled" : "")
-                                    + (skipError ? " managed_error" : "")
-                            }
-                            onClick={() => this.props.sendPlayerCommands({skip: true})}
-                            disabled={controlDisabled}
+                                <ReactCSSTransitionGroup
+                                    transitionName="managed"
+                                    transitionEnterTimeout={150}
+                                    transitionLeaveTimeout={150}
+                                >
+                                    {playPausebtn}
+                                </ReactCSSTransitionGroup>
+                            </button>
+                        </IsPlaylistManagerOrOwner>
+                        <IsPlaylistManagerOrOwner
+                            object={playerStatus.playlist_entry}
+                            disable
                         >
-                            <ReactCSSTransitionGroup
-                                transitionName="managed"
-                                transitionEnterTimeout={150}
-                                transitionLeaveTimeout={150}
+                            <button
+                                className={
+                                    "control primary"
+                                        + (skipError ? " managed_error" : "")
+                                }
+                                onClick={() => this.props.sendPlayerCommands({skip: true})}
+                                disabled={controlDisabled}
                             >
-                                {skipBtn}
-                        </ReactCSSTransitionGroup>
-                    </button>
-                </div>
-                <div className="song notifiable">
-                    {songData}
-                    {songOwner}
-                    <div className="song-timing">
-                        <div className="current">
-                            {utils.formatTime(playerStatus.timing)}
-                        </div>
-                        <div className="duration">
-                            {utils.formatDuration(duration)}
-                        </div>
+                                <ReactCSSTransitionGroup
+                                    transitionName="managed"
+                                    transitionEnterTimeout={150}
+                                    transitionLeaveTimeout={150}
+                                >
+                                    {skipBtn}
+                                </ReactCSSTransitionGroup>
+                            </button>
+                        </IsPlaylistManagerOrOwner>
                     </div>
-                    <ReactCSSTransitionGroup
-                        transitionName="notified"
-                        transitionEnterTimeout={300}
-                        transitionLeaveTimeout={150}
-                    >
-                        {notificationBanner}
-                    </ReactCSSTransitionGroup>
+                    <div className="song notifiable">
+                        {songData}
+                        {songOwner}
+                        <div className="song-timing">
+                            <div className="current">
+                                {utils.formatTime(playerStatus.timing)}
+                            </div>
+                            <div className="duration">
+                                {utils.formatDuration(duration)}
+                            </div>
+                        </div>
+                        <ReactCSSTransitionGroup
+                            transitionName="notified"
+                            transitionEnterTimeout={300}
+                            transitionLeaveTimeout={150}
+                        >
+                            {notificationBanner}
+                        </ReactCSSTransitionGroup>
+                    </div>
+                </div>
+                <div className="progressbar">
+                    <div className="progress" style={progressStyle}></div>
                 </div>
             </div>
-            <div className="progressbar">
-                <div className="progress" style={progressStyle}></div>
-            </div>
-        </div>
         )
     }
 }
--- src/jsx/containers/PlaylistPermissions.js
+++ src/jsx/containers/PlaylistPermissions.js
@@ -31,16 +31,15 @@ class BasePermission extends Component {
     render() {
         const { children, disable } = this.props
 
+        if (React.Children.count(children) > 1) {
+            throw new Error("Permission component should only have one child")
+        }
+
         // if the user does not have the permission to display the children
         if (!this.has_permission()) {
             // if the children can be disabled, display them disabled
             if (disable) {
-                // FIXME remove the div wrapper
-                return (
-                    <div>{React.Children.map(children, (child) => (
-                    React.cloneElement(child, {disabled: true})
-                    ))}</div>
-                )
+                return React.cloneElement(React.Children.only(children), {disabled: true})
             }
 
             // else, do not display them at all
@@ -48,7 +47,7 @@ class BasePermission extends Component {
         }
 
         // otherwize, if the user has permission to display the children
-        return this.props.children
+        return children
     }
 }
 
@@ -68,8 +67,15 @@ export const IsPlaylistManagerOrOwner = connect(
         has_permission_custom = () => {
             const { user, object } = this.props
 
-            return (user.playlist_permission_level == 'm' ||
-                user.id == object.owner.id)
+            if (user.playlist_permission_level == 'm') {
+                return true
+            }
+
+            if (!object) {
+                return false
+            }
+
+            return user.id == object.owner.id
         }
     }
 )
