0f30da12d37a3c105c7a6f45f2f85ccf19c6c53d
Neraste <neraste.herr10@gmail.com>
2018-01-09 00:30:37 +0100

    Fix Not Found and Forbidden pages

--- src/jsx/components/navigation/Forbidden.jsx
+++ src/jsx/components/navigation/Forbidden.jsx
@@ -5,8 +5,10 @@ export default class Forbidden extends Component {
         const url = this.props.location.pathname
         return (
                 <div className="box" id="error-page">
-                    <h2>Forbidden</h2>
-                    <div className="from-url">{url}</div>
+                    <div className="box-header">
+                        <h2>Forbidden</h2>
+                    </div>
+                    <div className="url">{url}</div>
                     <p>We're sorry, you do not have the privilege to access this
                     ressource</p>
                 </div>
--- src/jsx/components/navigation/NotFound.jsx
+++ src/jsx/components/navigation/NotFound.jsx
@@ -5,8 +5,10 @@ export default class NotFound extends Component {
         const url = this.props.location.pathname
         return (
                 <div className="box" id="error-page">
-                    <h2>Not found</h2>
-                    <div className="from-url">{url}</div>
+                    <div className="box-header">
+                        <h2>Not found</h2>
+                    </div>
+                    <div className="url">{url}</div>
                     <p>We're sorry, your request did not match any route…</p>
                 </div>
         )
