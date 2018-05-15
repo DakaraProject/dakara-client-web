0cc25ea380d676f805e008fa49dd6756631a58aa
Nadeflore <flore.love@gmail.com>
2018-05-15 19:14:38 +0200

    Add basic library tests

---
+++ cypress/integration/library_spec.js
@@ -0,0 +1,76 @@
+describe('Song library tests', function() {
+    beforeEach(function() {
+        // Stub api routes
+        cy.server()
+        cy.route('/api/users/current/', 'fixture:users/current.json')
+        cy.route('/api/library/work-types/', 'fixture:library/work-types.json')
+        cy.route('/api/library/songs/?page=1', 'fixture:library/songs_page1.json')
+        cy.route('/api/library/songs/?page=2', 'fixture:library/songs_page2.json')
+        cy.route('/api/library/songs/?page=3', 'fixture:library/songs_page3.json')
+        cy.route('/api/library/songs/?page=4', 'fixture:library/songs_page4.json')
+        cy.route('/api/library/songs/?page=1&query=*', 'fixture:library/songs_query.json').as('songs_query')
+        cy.route('/api/playlist/digest/', 'fixture:playlist/digest.json')
+        cy.route('/api/playlist/entries/', 'fixture:playlist/entries.json')
+        cy.route('/api/playlist/played-entries/', 'fixture:playlist/played-entries.json')
+
+        // visits home page
+        // with token set, to appear logged in
+        cy.visit('/', {
+            onBeforeLoad: win => {
+                win.localStorage.setItem("redux", JSON.stringify({token: "tokenString"}))
+            }
+        })
+    })
+
+    it('displays songs details in compact and expanded form', function() {
+        cy.url().should('include', '/library/song')
+
+        // check basic info about a song are displayed 
+        cy.get('.library-entry-song:nth-child(2)').should('be.visible')
+            .and('contain', 'BlackRunning') // title
+            .and('contain', 'versultimate version') // version
+            .and('contain', 'Super work') // work
+            .and('contain', 'OP2') // work link
+            .and('contain', 'The artist') // artist
+            .and('contain', 'Artist1') // artist
+            .and('contain', 'PV') // tag
+            .and('contain', 'INST') // tag
+
+        // expand listing entry
+        cy.get('.library-entry-song:nth-child(2) .song').click()
+
+        // check detailed info about a song are displayed 
+        cy.get('.library-entry-song:nth-child(2) .library-entry-song-expanded').should('be.visible')
+            .and('contain', 'Super work') // work
+            .and('contain', 'Opening2') // work link
+            .and('contain', 'The artist') // artist
+            .and('contain', 'Artist1') // artist
+            .and('contain', 'PV') // tag
+            .and('contain', 'INST') // tag
+            .and('contain', 'super detail') // detail
+            .and('contain', 'super video detai') // video detail
+            .and('contain', 'Lalalalalalala') // Lyrics
+
+
+    })
+
+    it('searches songs and highlight result', function() {
+        cy.url().should('include', '/library/song')
+
+        cy.get("#library-searchbox-fake input").type('black{enter}')
+
+        // check request is sent
+        cy.wait('@songs_query')
+            .its('url')
+            .should('contains', 'query=black')
+
+        // 2 results
+        cy.get('.library-entry-song').children().should('have.length', 2)
+
+        // check highlighting
+        cy.get('.library-entry-song:nth-child(1) mark').should('contain', 'Black')
+        cy.get('.library-entry-song:nth-child(2) mark').should('contain', 'Black')
+
+
+    })
+})
