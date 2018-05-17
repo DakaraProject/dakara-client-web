3b8b002336ff124fb62cdab48fce83db42b4fceb
Neraste <neraste.herr10@gmail.com>
2018-05-17 03:34:54 +0200

    Magnetise the #content div
    Make the scroll to be attracted by the top border of the component.

--- src/jsx/components/Main.jsx
+++ src/jsx/components/Main.jsx
@@ -10,6 +10,8 @@ import { IsAuthenticated } from './permissions/Base'
 import { loadWorkTypes } from 'actions/library'
 
 class Main extends Component {
+    playlistAppWrapperRef = React.createRef()
+
     static propTypes = {
         isLoggedIn: PropTypes.bool.isRequired,
         loadCurrentUser: PropTypes.func.isRequired,
@@ -34,14 +36,17 @@ class Main extends Component {
         return (
             <div id="main">
                 <Header/>
-                <div id="playlist-app-wrapper">
+                <div id="playlist-app-wrapper" ref={this.playlistAppWrapperRef}>
                     <IsAuthenticated>
                         <PlaylistApp/>
                     </IsAuthenticated>
                 </div>
-                <div id="content">
+                <Content
+                    thresold={50}
+                    playlistAppWrapperRef={this.playlistAppWrapperRef}
+                >
                     {this.props.children}
-                </div>
+                </Content>
                 <Footer/>
             </div>
         )
@@ -61,3 +66,98 @@ Main = withRouter(connect(
 )(Main))
 
 export default Main
+
+
+/**
+ * Content component
+ *
+ * This component bahaves like a magnet for the scrollbar. When the user scrolls
+ * passed to a certain thresold arount its top berder, the window is attracted to
+ * it and scrolls smoothly to its position. To avoid a "scroll trap", the window
+ * scrolls only if the user is scrolling in the appropriate way (approaching the
+ * top border, not going away from it).
+ */
+class Content extends Component {
+    elementRef = React.createRef()
+
+    state = {
+        latestPosition: 0,
+    }
+
+    static propTypes = {
+        thresold: PropTypes.number.isRequired,
+        playlistAppWrapperRef: PropTypes.shape({
+            current: PropTypes.any
+        }).isRequired
+    }
+
+    static defaultProps = {
+        thresold: 10,
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.onScroll, false)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.onScroll, false)
+    }
+
+    onScroll = (e) => {
+        const { thresold, playlistAppWrapperRef } = this.props
+        const { latestPosition, latestTime } = this.state
+        const element = this.elementRef.current
+        const playlistAppWrapper = playlistAppWrapperRef.current
+        const currentPosition = window.pageYOffset
+
+        // save position
+        this.setState({latestPosition: currentPosition})
+
+        // if the playlist app wrapper is unreachable, do nothing
+        if (!playlistAppWrapper) return
+
+        // direction of scroll
+        const deltaPosition = currentPosition - latestPosition
+
+        // height of the playlist app wrapper, which offsets the location of the
+        // component
+        const playlistAppWrapperHeight = playlistAppWrapper.clientHeight
+        const location = element.offsetTop - playlistAppWrapperHeight
+
+        // if we descend to the top border of the component and are within the
+        // upper thresold, do scroll
+        if (currentPosition > location - thresold &&
+            currentPosition < location &&
+            deltaPosition > 0) {
+
+            window.scroll({
+                top: location,
+                behavior: 'smooth',
+            })
+
+            return
+        }
+
+        // if we ascend to the top border of the component and are within the
+        // lower thresold, do scroll
+        if (currentPosition > location &&
+            currentPosition < location + thresold &&
+            deltaPosition < 0) {
+
+            window.scroll({
+                top: location,
+                behavior: 'smooth',
+            })
+
+            return
+        }
+    }
+
+    render() {
+        return (
+            <div id="content" ref={this.elementRef}>
+                {this.props.children}
+            </div>
+        )
+    }
+}
