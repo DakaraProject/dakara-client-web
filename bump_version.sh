2d75e5794058391d33e7802af05ab1d25a9b566e
Neraste <neraste.herr10@gmail.com>
2018-06-03 18:54:31 +0200

    Update bump version script
    Now creates the commit by itself.

--- bump_version.sh
+++ bump_version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [[ -z $1 ]]
 then
     >&2 echo 'Error: no version specified'
@@ -11,9 +13,13 @@ version_date=$(date -I -u)
 
 npm version $version_number --no-git-tag-version
 
+changelog_file=CHANGELOG.md
 sed -i "/^## Unreleased$/a \\
 \\
-## $version_number - $version_date" CHANGELOG.md 
+## $version_number - $version_date" $changelog_file
+
+git add package.json $changelog_file
+git commit -m "Version $version_number"
+git tag $version_number
 
 echo "Version bumped to $version_number"
-
