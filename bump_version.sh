dce684347dbaff52348323a0e0acf86437b89708
Nadeflore <flore.love@gmail.com>
2019-05-03 15:56:34 +0900

    Update bump_version script for dev version
    Script now creates new commit for dev version after release

--- bump_version.sh
+++ bump_version.sh
@@ -8,10 +8,18 @@ then
     exit 1
 fi
 
+# next server version
+if [[ -z $2 ]]
+then
+    >&2 echo 'Error: no next version specified'
+    exit 1
+fi
+
 version_number=$1
+dev_version_number=$2-dev
 version_date=$(date -I -u)
 
-npm version $version_number --no-git-tag-version
+npm version "$version_number" --no-git-tag-version
 
 changelog_file=CHANGELOG.md
 sed -i "/^## Unreleased$/a \\
@@ -20,6 +28,15 @@ sed -i "/^## Unreleased$/a \\
 
 git add package.json package-lock.json $changelog_file
 git commit -m "Version $version_number"
-git tag $version_number
+git tag "$version_number"
 
 echo "Version bumped to $version_number"
+
+# patch version file for dev version
+npm version "$dev_version_number" --no-git-tag-version
+# create commit
+git add package.json package-lock.json
+git commit -m "Dev version $dev_version_number" --no-verify
+
+# say something
+echo "Updated to dev version $dev_version_number"
