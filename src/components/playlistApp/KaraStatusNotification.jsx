92d2b024e24c09bea0b8b502728396724fc1a489
Neraste <neraste.herr10@gmail.com>
2021-08-14 23:29:34 +0900

    Fix kara status notification

--- src/components/playlistApp/KaraStatusNotification.jsx
+++ src/components/playlistApp/KaraStatusNotification.jsx
@@ -3,17 +3,21 @@ import { Link } from 'react-router-dom'
 
 const karaStatusNotification = () => (
     <div className="box" id="kara-status-notification">
-        <div className="message">
-            The karaoke is stopped for now. You can activate
-            it in the settings page.
-        </div>
-        <div className="controls">
-            <Link
-                className="control primary"
-                to="/settings/kara-status"
-            >
-                Go to settings page
-            </Link>
+        <div className="content">
+            <div className="ribbon primary">
+                <p className="message">
+                    The karaoke is stopped for now.
+                    You can activate it in the settings page.
+                </p>
+                <div className="controls">
+                    <Link
+                        className="control primary"
+                        to="/settings/kara-status"
+                    >
+                        Go to settings page
+                    </Link>
+                </div>
+            </div>
         </div>
     </div>
 )
