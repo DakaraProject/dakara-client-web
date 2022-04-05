0815b041963c362170101f7455ec6501ab47c2de
Neraste <neraste.herr10@gmail.com>
2022-04-05 23:50:53 +0900

    Use an actual warning (and better)

--- src/components/DevWarning.jsx
+++ src/components/DevWarning.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import semver from 'semver'
 
-export default class DevWarning extends Component {
-    componentDidMount() {
-        console.log('You are running a dev version, use it at your own risks!')
-    }
 
+export default class DevWarning extends Component {
     render() {
         const version = semver.parse(process.env.DAKARA_VERSION)
 
         if (version.prerelease.length > 0) {
+            console.warn('You are running a dev version, use it at your own risks!')
             return (
                 <div id="dev-warning"></div>
             )
