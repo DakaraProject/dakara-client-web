f4ffaf69f379107724b36462f0d6b074f193f0aa
NextFire <git@yuru.moe>
2024-02-01 16:19:46 -0500

    Vite migration

--- create_archive.sh
+++ create_archive.sh
@@ -17,15 +17,7 @@ archive_name=dakara-client-web_$version_number.zip
 echo "Creating build, please wait..."
 npm run build
 
-
-# Go to build directory
-cd build
-
-# Move index.html to static dir
-mv index.html static
-
-# archive the static folder
-zip -r ../"$archive_name" static
+# archive the dist folder
+zip -r "$archive_name" dist
 
 echo "Archive created in $archive_name"
-
