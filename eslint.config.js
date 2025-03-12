5d19eb2681ca94d5cdb003438ead62ace800ae04
Neraste <neraste.herr10@gmail.com>
2025-03-13 00:54:10 +0100

    Format config files
    Allow eslint to run in root directory.

--- eslint.config.js
+++ eslint.config.js
@@ -1,78 +1,85 @@
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
-import importPlugin from 'eslint-plugin-import'
-import globals from 'globals'
 import js from '@eslint/js'
-import prettier from "eslint-plugin-prettier/recommended"
-import react from 'eslint-plugin-react'
+import importPlugin from 'eslint-plugin-import'
+import prettierPlugin from 'eslint-plugin-prettier/recommended'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import reactRefreshPlugin from 'eslint-plugin-react-refresh'
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
 
 export default [
   {
-    ignores: [
-      'dist',
-      'node_modules',
-    ],
+    ignores: ['dist', 'node_modules'],
   },
   js.configs.recommended,
   importPlugin.flatConfigs.recommended,
-  react.configs.flat.recommended,
-  react.configs.flat['jsx-runtime'],
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
   {
-    files: [
-        '**/*.{js,jsx}'
-    ],
+    files: ['**/*.{js,jsx}'],
 
     languageOptions: {
-        globals: globals.browser,
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
 
     plugins: {
-        'react-hooks': reactHooks,
-        'react-refresh': reactRefresh,
-        'simple-import-sort': simpleImportSort,
+      'react-hooks': reactHooksPlugin,
+      'react-refresh': reactRefreshPlugin,
+      'simple-import-sort': simpleImportSortPlugin,
     },
 
     settings: {
-        react: {
-            version: 'detect'
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx'],
+          moduleDirectory: ['node_modules', 'src/'],
         },
-        "import/resolver": {
-            node: {
-              extensions: [".js", ".jsx"],
-              moduleDirectory: ['node_modules', 'src/'],
-            },
-        },
+      },
     },
 
     rules: {
-        ...reactHooks.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
 
-        'react-refresh/only-export-components': ['warn', {
-            allowConstantExport: true,
-        }],
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
 
-        'no-class-assign': 'off',
-        'no-unused-vars': [
-            'error',
-            {
-                args: 'none',
-            }
-        ],
+      'no-class-assign': 'off',
+      'no-unused-vars': [
+        'error',
+        {
+          args: 'none',
+        },
+      ],
 
-        'simple-import-sort/imports': ['error', {
-            groups: [['^\\u0000'], ['^@?\\w'], [
-                '^(actions|components|eventManagers|middleware|permissions|reducers|serverPropTypes|style|thirdpartyExtensions|utils)',
-            ], ['^'], ['^\\.']],
-        }],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^\\u0000'],
+            ['^@?\\w'],
+            [
+              '^(actions|components|eventManagers|middleware|permissions|reducers|serverPropTypes|style|thirdpartyExtensions|utils)',
+            ],
+            ['^'],
+            ['^\\.'],
+          ],
+        },
+      ],
 
-        'import/first': 'error',
-        'import/newline-after-import': 'error',
-        'import/no-duplicates': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
 
-        quotes: ['error', 'single'],
+      quotes: ['error', 'single'],
     },
-  }
-].concat(prettier)
+  },
+].concat(prettierPlugin)
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,14 +1,14 @@
 export default {
   extends: [
-    "stylelint-config-sass-guidelines",
-    "stylelint-prettier/recommended",
+    'stylelint-config-sass-guidelines',
+    'stylelint-prettier/recommended',
   ],
   rules: {
-    "color-named": "always-where-possible",
-    "max-nesting-depth": 99,
-    "scss/selector-no-redundant-nesting-selector": null,
-    "selector-max-compound-selectors": 9,
-    "selector-max-id": 1,
-    "selector-no-qualifying-type": [null, { "ignore": ["attribute", "class"] }],
-  }
+    'color-named': 'always-where-possible',
+    'max-nesting-depth': 99,
+    'scss/selector-no-redundant-nesting-selector': null,
+    'selector-max-compound-selectors': 9,
+    'selector-max-id': 1,
+    'selector-no-qualifying-type': [null, { ignore: ['attribute', 'class'] }],
+  },
 }
