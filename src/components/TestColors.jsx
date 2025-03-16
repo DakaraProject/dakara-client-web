b04b03b372b0bd0b2fabd92cfa250b8cba5ef432
Neraste <neraste.herr10@gmail.com>
2025-03-16 02:17:18 +0100

    Add contrasts section to colors test

--- src/components/TestColors.jsx
+++ src/components/TestColors.jsx
@@ -2,6 +2,13 @@ import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import { useEffect, useRef, useState } from 'react'
 
+function getColor(ref, colorName) {
+  const str = window.getComputedStyle(ref.current)[colorName]
+  const regex = /rgb\((\d+), (\d+), (\d+)\)/
+  const [_, r, g, b] = str.match(regex)
+  return [parseInt(r), parseInt(g), parseInt(b)]
+}
+
 // see:
 // https://blog.logrocket.com/how-to-manipulate-css-colors-with-javascript-fb547113a1b8/
 const rgbToLightness = (r, g, b) =>
@@ -26,19 +33,19 @@ const rgbToHsl = (r, g, b) => {
   return [hue, saturation, lightness]
 }
 
-const formatRgbIntToHsl = (rInt, gInt, bInt) => {
+const formatRgbIntToHsl = ([rInt, gInt, bInt]) => {
   const [h, s, l] = rgbToHsl(rInt / 255, gInt / 255, bInt / 255)
   return `${(360 + h) % 360}, ${s.toFixed(2)}, ${l.toFixed(2)}`
 }
 
-const formatRgbIntToHex = (rInt, gInt, bInt) => {
+const formatRgbIntToHex = ([rInt, gInt, bInt]) => {
   const r = rInt.toString(16).padStart(2, '0')
   const g = gInt.toString(16).padStart(2, '0')
   const b = bInt.toString(16).padStart(2, '0')
   return `#${r}${g}${b}`
 }
 
-const formatRgbIntToRgb = (rInt, gInt, bInt) => {
+const formatRgbIntToRgb = ([rInt, gInt, bInt]) => {
   const r = rInt / 255
   const g = gInt / 255
   const b = bInt / 255
@@ -51,50 +58,50 @@ const formatters = {
   hsl: formatRgbIntToHsl,
 }
 
-function Tile({ color, format, refresher }) {
+function MosaicTile({ color, format, refresher }) {
   // get the color of the div and convert it to HSL space
   const ref = useRef(null)
-  const [background, setBackground] = useState('')
+  const [text, setText] = useState('')
   useEffect(() => {
-    const bgStr = window.getComputedStyle(ref.current)['background']
-    const regex = /rgb\((\d+), (\d+), (\d+)\)/
-    const [_, rStr, gStr, bStr] = bgStr.match(regex)
-    setBackground(
-      formatters[format](parseInt(rStr), parseInt(gStr), parseInt(bStr))
-    )
+    setText(formatters[format](getColor(ref, 'background')))
   }, [format, refresher])
   return (
     <td className={classNames('tile', color)} ref={ref}>
-      {background}
+      {text}
     </td>
   )
 }
 
-Tile.propTypes = {
+MosaicTile.propTypes = {
   color: PropTypes.string,
   format: PropTypes.string,
   refresher: PropTypes.number,
 }
 
-function Row({ name, colors, format, refresher }) {
+function MosaicRow({ name, colors, format, refresher }) {
   return (
     <tr className="listing-entry">
-      <td className="tile title">{name}</td>
+      <td className="title">{name}</td>
       {colors.map((color) => (
-        <Tile color={color} key={color} format={format} refresher={refresher} />
+        <MosaicTile
+          color={color}
+          key={color}
+          format={format}
+          refresher={refresher}
+        />
       ))}
     </tr>
   )
 }
 
-Row.propTypes = {
+MosaicRow.propTypes = {
   name: PropTypes.string,
   colors: PropTypes.arrayOf(PropTypes.string),
   format: PropTypes.string,
   refresher: PropTypes.number,
 }
 
-export default function TestColors() {
+function Mosaic() {
   // add a manual refresher for the HSL values displayed in the tiles that
   // won't refresh themselves when the CSS is updated
   const [refresher, setRefresher] = useState(0)
@@ -137,55 +144,194 @@ export default function TestColors() {
     '413',
   ]
 
-  const controls = (
-    <div className="controls">
-      <button className="control primary" onClick={createSetFormat('hex')}>
-        Show HEX
-      </button>
-      <button className="control primary" onClick={createSetFormat('rgb')}>
-        Show RGB
-      </button>
-      <button className="control primary" onClick={createSetFormat('hsl')}>
-        Show HSL
-      </button>
-      <button className="control primary" onClick={forceRefresh}>
-        Refresh
-      </button>
+  return (
+    <div className="section">
+      <h3>Colors</h3>
+      <table className="listing mosaic">
+        <thead>
+          <tr className="listing-header">
+            <th>Lightness</th>
+            <th>Primary</th>
+            <th>Neutral</th>
+            <th>Success</th>
+            <th>Warning</th>
+            <th>Danger</th>
+            <th>Info</th>
+          </tr>
+        </thead>
+        <tbody>
+          {lightnesses.map((lightness) => (
+            <MosaicRow
+              key={lightness}
+              name={lightness}
+              colors={colors.map((color) => `${color}-${lightness}`)}
+              format={format}
+              refresher={refresher}
+            />
+          ))}
+        </tbody>
+      </table>
+      <div className="controls">
+        <button className="control primary" onClick={createSetFormat('hex')}>
+          Show HEX
+        </button>
+        <button className="control primary" onClick={createSetFormat('rgb')}>
+          Show RGB
+        </button>
+        <button className="control primary" onClick={createSetFormat('hsl')}>
+          Show HSL
+        </button>
+        <button className="control primary" onClick={forceRefresh}>
+          Refresh
+        </button>
+      </div>
     </div>
   )
+}
 
+// see: https://stackoverflow.com/a/9733420
+const RED = 0.2126
+const GREEN = 0.7152
+const BLUE = 0.0722
+
+const GAMMA = 2.4
+
+function luminance(r, g, b) {
+  var a = [r, g, b].map((v) => {
+    v /= 255
+    return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, GAMMA)
+  })
+  return a[0] * RED + a[1] * GREEN + a[2] * BLUE
+}
+
+function contrast(rgb1, rgb2) {
+  var lum1 = luminance(...rgb1)
+  var lum2 = luminance(...rgb2)
+  var brightest = Math.max(lum1, lum2)
+  var darkest = Math.min(lum1, lum2)
+  return (brightest + 0.05) / (darkest + 0.05)
+}
+
+function SamplerTile({ background, foreground, refresher }) {
+  const ref = useRef(null)
+  const [text, setText] = useState('')
+  useEffect(() => {
+    setText(
+      contrast(getColor(ref, 'background'), getColor(ref, 'color')).toFixed(2)
+    )
+  }, [refresher])
+  return (
+    <td
+      className={classNames('tile', background, `fg-${foreground}`)}
+      ref={ref}
+    >
+      {text}
+    </td>
+  )
+}
+
+SamplerTile.propTypes = {
+  background: PropTypes.string,
+  foreground: PropTypes.string,
+  refresher: PropTypes.number,
+}
+
+function SamplerRow({ background, foreground, colors, refresher }) {
+  return (
+    <tr className="listing-entry">
+      <td className="title">{background}</td>
+      <td className="title">{foreground}</td>
+      {colors.map((color) => (
+        <SamplerTile
+          background={`${color}-${background}`}
+          foreground={`${color}-${foreground}`}
+          key={color}
+          refresher={refresher}
+        />
+      ))}
+    </tr>
+  )
+}
+
+SamplerRow.propTypes = {
+  background: PropTypes.string,
+  foreground: PropTypes.string,
+  colors: PropTypes.arrayOf(PropTypes.string),
+  refresher: PropTypes.number,
+}
+
+function Sampler() {
+  const [refresher, setRefresher] = useState(0)
+
+  const forceRefresh = () => {
+    setRefresher((refresher) => refresher + 1)
+  }
+
+  const colors = ['primary', 'neutral', 'success', 'warning', 'danger', 'info']
+
+  const lightnesses = {
+    '000': ['000'],
+    100: ['100', '101', '102', '110', '111', '112', '120', '121', '122'],
+    200: ['200', '201', '202'],
+    300: ['300', '301', '302', '303'],
+    400: ['400', '402', '403', '410', '412', '413'],
+  }
+
+  const createRows = (...arrays) =>
+    arrays.reduce((aBg, aFg) =>
+      aBg.flatMap((bg) =>
+        aFg.map((fg) => (
+          <SamplerRow
+            key={`${bg}-${fg}`}
+            background={bg}
+            foreground={fg}
+            colors={colors}
+            refresher={refresher}
+          />
+        ))
+      )
+    )
+
+  return (
+    <div className="section">
+      <h3>Contrasts</h3>
+      <table className="listing sampler">
+        <thead>
+          <tr className="listing-header">
+            <th>Background lightness</th>
+            <th>Foreground lightness</th>
+            <th>Primary</th>
+            <th>Neutral</th>
+            <th>Success</th>
+            <th>Warning</th>
+            <th>Danger</th>
+            <th>Info</th>
+          </tr>
+        </thead>
+        <tbody>
+          {createRows(lightnesses['200'], lightnesses['000'])}
+          {createRows(lightnesses['300'], lightnesses['000'])}
+          {createRows(lightnesses['100'], lightnesses['400'])}
+        </tbody>
+      </table>
+      <div className="controls">
+        <button className="control primary" onClick={forceRefresh}>
+          Refresh
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default function TestColors() {
   return (
     <div className="test-colors box">
       <div className="header">
         <h2>Test colors</h2>
       </div>
       <div className="content">
-        <h3>Colors</h3>
-        <table className="listing mosaic">
-          <thead>
-            <tr className="listing-header">
-              <th>Lightness</th>
-              <th>Primary</th>
-              <th>Neutral</th>
-              <th>Success</th>
-              <th>Warning</th>
-              <th>Danger</th>
-              <th>Info</th>
-            </tr>
-          </thead>
-          <tbody>
-            {lightnesses.map((lightness) => (
-              <Row
-                key={lightness}
-                name={lightness}
-                colors={colors.map((color) => `${color}-${lightness}`)}
-                format={format}
-                refresher={refresher}
-              />
-            ))}
-          </tbody>
-        </table>
-        {controls}
+        <Mosaic />
+        <Sampler />
       </div>
     </div>
   )
