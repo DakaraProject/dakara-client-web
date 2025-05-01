cbe56d9ecf11868148795f9e0c9ae773319b6ee7
Neraste <neraste.herr10@gmail.com>
2025-05-02 01:36:08 +0200

    Add benchmarks

---
+++ src/utils/index.bench.js
@@ -0,0 +1,31 @@
+import { bench, describe } from 'vitest'
+
+import { getEntriesHash } from '.'
+
+describe('get entries hash', () => {
+  const entries = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 },
+    { id: 10 },
+    { id: 11 },
+    { id: 12 },
+    { id: 13 },
+    { id: 14 },
+    { id: 15 },
+    { id: 16 },
+    { id: 17 },
+    { id: 18 },
+    { id: 19 },
+    { id: 20 },
+  ]
+  bench('generate for 20 entries', () => {
+    getEntriesHash(entries)
+  })
+})
