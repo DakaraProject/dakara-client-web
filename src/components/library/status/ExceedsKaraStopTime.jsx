819487422b7a71cd62823200e79dc0718e0ba67c
Neraste <neraste.herr10@gmail.com>
2025-05-03 20:29:31 +0200

    Fix wrong display of exceeds kara stop time status

--- src/components/library/status/ExceedsKaraStopTime.jsx
+++ src/components/library/status/ExceedsKaraStopTime.jsx
@@ -12,7 +12,7 @@ export default function ExceedsKaraStopTime({ expanded }) {
   }
   return (
     <li
-      className={classNames('song exceeds-kara-stop-time warning', {
+      className={classNames('status exceeds-kara-stop-time warning', {
         'expanded listable': expanded,
       })}
     >
