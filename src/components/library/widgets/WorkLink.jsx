5ea165ef0fc7c4da2569fd77f752c3ef789f06ee
Neraste <neraste.herr10@gmail.com>
2025-05-11 02:27:51 +0200

    Refactor WorkLink widget as a functional component

--- src/components/library/widgets/WorkLink.jsx
+++ src/components/library/widgets/WorkLink.jsx
@@ -1,63 +1,64 @@
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
-import { Component } from 'react'
 
 import WorkWidget from 'components/library/widgets/Work'
 import { WorkLinkName } from 'reducers/library'
 import { workLinkPropType } from 'serverPropTypes/library'
 
-export default class WorkLinkWidget extends Component {
-  static propTypes = {
-    workLink: workLinkPropType.isRequired,
-    query: PropTypes.object,
-    longLinkType: PropTypes.bool,
-    noEpisodes: PropTypes.bool,
-    truncatable: PropTypes.bool,
-  }
+export default function WorkLinkWidget(
+  workLink,
+  query,
+  longLinkType,
+  noEpisodes,
+  truncatable,
+  ...rest
+) {
+  /**
+   * Link
+   */
 
-  render() {
-    const { workLink, query, longLinkType, noEpisodes, truncatable, ...rest } =
-      this.props
+  const linkType = (
+    <span className="link-type">
+      {longLinkType ? WorkLinkName[workLink.link_type] : workLink.link_type}
+    </span>
+  )
 
-    /**
-     * Link
-     */
-
-    const linkType = (
-      <span className="link-type">
-        {longLinkType ? WorkLinkName[workLink.link_type] : workLink.link_type}
-      </span>
-    )
-
-    let linkNumber
-    if (workLink.link_type_number) {
-      linkNumber = (
-        <span className="link-number">{workLink.link_type_number}</span>
-      )
-    }
-
-    const link = (
-      <span className="link">
-        {linkType}
-        {linkNumber}
-      </span>
-    )
-
-    /**
-     * Episodes
-     */
-
-    let episodes
-    if (!noEpisodes && workLink.episodes) {
-      episodes = <span className="episodes">Episode {workLink.episodes}</span>
-    }
-
-    return (
-      <div className={classNames('work-link-widget', { truncatable })}>
-        <WorkWidget work={workLink.work} query={query} {...rest} noCount />
-        {link}
-        {episodes}
-      </div>
+  let linkNumber
+  if (workLink.link_type_number) {
+    linkNumber = (
+      <span className="link-number">{workLink.link_type_number}</span>
     )
   }
+
+  const link = (
+    <span className="link">
+      {linkType}
+      {linkNumber}
+    </span>
+  )
+
+  /**
+   * Episodes
+   */
+
+  let episodes
+  if (!noEpisodes && workLink.episodes) {
+    episodes = <span className="episodes">Episode {workLink.episodes}</span>
+  }
+
+  return (
+    <div className={classNames('work-link-widget', { truncatable })}>
+      <WorkWidget work={workLink.work} query={query} {...rest} noCount />
+      {link}
+      {episodes}
+    </div>
+  )
+}
+
+WorkLinkWidget.propTypes = {
+  workLink: workLinkPropType.isRequired,
+  query: PropTypes.object,
+  longLinkType: PropTypes.bool,
+  noEpisodes: PropTypes.bool,
+  truncatable: PropTypes.bool,
 }
