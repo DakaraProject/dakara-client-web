95c11179ed2a203d05bef7547fd23269647675b9
Neraste <neraste.herr10@gmail.com>
2025-11-23 02:55:11 +0100

    Convert Player to functional component

--- src/components/karaoke/player/Player.jsx
+++ src/components/karaoke/player/Player.jsx
@@ -1,8 +1,6 @@
 import classNames from 'classnames'
-import PropTypes from 'prop-types'
-import queryString from 'query-string'
-import { Component } from 'react'
-import { connect } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { sendPlayerCommand } from 'actions/playlist'
 import { Carousel } from 'components/generics/Carousel'
@@ -14,206 +12,157 @@ import {
 import ManageButton from 'components/karaoke/player/ManageButton'
 import PlayerNotification from 'components/karaoke/player/Notification'
 import { IsPlaylistManagerOrOwner } from 'permissions/Playlist'
-import {
-  alterationResponsePropType,
-  Status,
-} from 'reducers/alterationsResponse'
-import { playerStatusStatePropType } from 'reducers/playlist'
-import { playerErrorsDigestStatePropType } from 'reducers/playlistDigest'
-import { userPropType } from 'serverPropTypes/users'
-import { withNavigate } from 'thirdpartyExtensions/ReactRouterDom'
+import { Status } from 'reducers/alterationsResponse'
 import { CSSTransitionLazy } from 'thirdpartyExtensions/ReactTransitionGroup'
 
-class Player extends Component {
-  static propTypes = {
-    playerErrorsDigestState: playerErrorsDigestStatePropType.isRequired,
-    playerStatusState: playerStatusStatePropType.isRequired,
-    responseOfSendPlayerCommands: PropTypes.objectOf(
-      alterationResponsePropType
-    ),
-    sendPlayerCommand: PropTypes.func.isRequired,
-    user: userPropType.isRequired,
-    navigate: PropTypes.func.isRequired,
+export default function Player() {
+  const user = useSelector((state) => state.authenticatedUser)
+  const playerStatusState = useSelector((state) => state.playlist.playerStatus)
+  const playerErrorsDigestState = useSelector(
+    (state) => state.playlist.digest.playerErrors
+  )
+  const responseOfSendPlayerCommands = useSelector(
+    (state) => state.alterationsResponse.multiple.sendPlayerCommands
+  )
+
+  const [animationsEnabled, setAnimationsEnabled] = useState(false)
+
+  const dispatch = useDispatch()
+
+  const { data: playerStatus } = playerStatusState
+  const withControls = IsPlaylistManagerOrOwner.hasPermission(
+    user,
+    playerStatus.playlist_entry
+  )
+
+  useEffect(() => {
+    // enable animations if the user, the player status, or the possibility to
+    // use controls changes
+    setAnimationsEnabled(true)
+  }, [user, playerStatus, withControls])
+
+  const { playerErrors } = playerErrorsDigestState.data
+  const fetchError = playerStatusState.status === Status.failed
+  const isPlaying = !!playerStatus.playlist_entry
+  const controlDisabled = !isPlaying || fetchError
+
+  // create a safe version of the send player commands alteration status with
+  // default values
+  const responseOfSendPlayerCommandsSafe = {
+    pause: { status: null },
+    resume: { status: null },
+    restart: { status: null },
+    skip: { status: null },
+    rewind: { status: null },
+    fast_forward: { status: null },
+    ...responseOfSendPlayerCommands,
   }
 
-  state = {
-    withControls: false,
-    animationsEnabled: false,
-  }
-
-  componentDidMount() {
-    const { user } = this.props
-    const { data: playerStatus } = this.props.playerStatusState
-    const withControls = IsPlaylistManagerOrOwner.hasPermission(
-      user,
-      playerStatus.playlist_entry
-    )
-
-    if (withControls) {
-      this.setState({ withControls })
+  // display playlist entry if any song is currently playing
+  let progress = 0
+  if (isPlaying) {
+    const { song } = playerStatus.playlist_entry
+    if (!playerStatus.in_transition && song.duration > 0) {
+      progress = Math.min(playerStatus.timing / song.duration, 1)
+    } else {
+      progress = undefined
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    const { user: prevUser } = prevProps
-    const { user } = this.props
-    const { data: playerStatus } = this.props.playerStatusState
-    const { data: prevPlayerStatus } = prevProps.playerStatusState
-
-    // display controls or not
-    if (user !== prevUser || playerStatus !== prevPlayerStatus) {
-      const { withControls: prevWithControls } = this.state
-      const withControls = IsPlaylistManagerOrOwner.hasPermission(
-        user,
-        playerStatus.playlist_entry
-      )
-
-      if (withControls !== prevWithControls) {
-        this.setState({ withControls, animationsEnabled: true })
-      }
-    }
-  }
-
-  handleSearch = (song) => {
-    const query = `title:""${song.title}""`
-    this.props.navigate({
-      pathname: '/library/song',
-      search: queryString.stringify({
-        query,
-        expanded: song.id,
-      }),
-    })
-  }
-
-  render() {
-    const { withControls, animationsEnabled } = this.state
-    const { data: playerStatus } = this.props.playerStatusState
-    const { playerErrors } = this.props.playerErrorsDigestState.data
-    const fetchError = this.props.playerStatusState.status === Status.failed
-    const isPlaying = !!playerStatus.playlist_entry
-    const controlDisabled = !isPlaying || fetchError
-
-    /**
-     * Create a safe version of the send player commands alteration status
-     * with default values
-     */
-
-    const responseOfSendPlayerCommandsSafe = {
-      pause: { status: null },
-      resume: { status: null },
-      restart: { status: null },
-      skip: { status: null },
-      rewind: { status: null },
-      fast_forward: { status: null },
-      ...this.props.responseOfSendPlayerCommands,
-    }
-
-    /**
-     * Display playlist entry if any song is currently playing
-     */
-
-    let progress = 0
-    if (isPlaying) {
-      const { song } = playerStatus.playlist_entry
-      if (!playerStatus.in_transition && song.duration > 0) {
-        progress = Math.min(playerStatus.timing / song.duration, 1)
-      } else {
-        progress = undefined
-      }
-    }
-
-    return (
-      <div
-        id="player"
-        className={classNames('box', fetchError ? 'danger' : 'primary')}
+  return (
+    <div
+      id="player"
+      className={classNames('box', fetchError ? 'danger' : 'primary')}
+    >
+      {fetchError ? (
+        <ServerLost />
+      ) : (
+        <Carousel className={fetchError ? 'danger' : 'primary'}>
+          <CarouselEntryCurrentSong />
+          <CarouselEntryNextSong />
+          <CarouselEntryStats />
+        </Carousel>
+      )}
+      <CSSTransitionLazy
+        in={withControls}
+        classNames="expand"
+        timeout={{
+          enter: 300,
+          exit: 150,
+        }}
+        enter={animationsEnabled}
+        exit={animationsEnabled}
       >
-        {fetchError ? (
-          <ServerLost />
-        ) : (
-          <Carousel className={fetchError ? 'danger' : 'primary'}>
-            <CarouselEntryCurrentSong />
-            <CarouselEntryNextSong />
-            <CarouselEntryStats />
-          </Carousel>
-        )}
-        <CSSTransitionLazy
-          in={withControls}
-          classNames="expand"
-          timeout={{
-            enter: 300,
-            exit: 150,
-          }}
-          enter={animationsEnabled}
-          exit={animationsEnabled}
-        >
-          <div className="controls">
-            <ManageButton
-              responseOfManage={responseOfSendPlayerCommandsSafe.restart}
-              onClick={() => this.props.sendPlayerCommand('restart')}
-              disabled={controlDisabled}
-              error={fetchError}
-              icon="step-backward"
-            />
-            <ManageButton
-              responseOfManage={responseOfSendPlayerCommandsSafe.rewind}
-              onClick={() => this.props.sendPlayerCommand('rewind')}
-              disabled={controlDisabled}
-              error={fetchError}
-              icon="backward"
-            />
-            <ManageButton
-              responseOfManage={
-                playerStatus.paused
-                  ? responseOfSendPlayerCommandsSafe.resume
-                  : responseOfSendPlayerCommandsSafe.pause
-              }
-              onClick={() => {
-                if (!isPlaying) return
+        <div className="controls">
+          <ManageButton
+            responseOfManage={responseOfSendPlayerCommandsSafe.restart}
+            onClick={() => {
+              dispatch(sendPlayerCommand('restart'))
+            }}
+            disabled={controlDisabled}
+            error={fetchError}
+            icon="step-backward"
+          />
+          <ManageButton
+            responseOfManage={responseOfSendPlayerCommandsSafe.rewind}
+            onClick={() => {
+              dispatch(sendPlayerCommand('rewind'))
+            }}
+            disabled={controlDisabled}
+            error={fetchError}
+            icon="backward"
+          />
+          <ManageButton
+            responseOfManage={
+              playerStatus.paused
+                ? responseOfSendPlayerCommandsSafe.resume
+                : responseOfSendPlayerCommandsSafe.pause
+            }
+            onClick={() => {
+              if (!isPlaying) return
 
-                if (playerStatus.paused) {
-                  this.props.sendPlayerCommand('resume')
-                } else {
-                  this.props.sendPlayerCommand('pause')
-                }
-              }}
-              disabled={controlDisabled}
-              error={fetchError}
-              icon={
-                isPlaying ? (playerStatus.paused ? 'play' : 'pause') : 'stop'
+              if (playerStatus.paused) {
+                dispatch(sendPlayerCommand('resume'))
+              } else {
+                dispatch(sendPlayerCommand('pause'))
               }
-            />
-            <ManageButton
-              responseOfManage={responseOfSendPlayerCommandsSafe.fast_forward}
-              onClick={() => this.props.sendPlayerCommand('fast_forward')}
-              disabled={controlDisabled}
-              error={fetchError}
-              icon="forward"
-            />
-            <ManageButton
-              responseOfManage={responseOfSendPlayerCommandsSafe.skip}
-              onClick={() => this.props.sendPlayerCommand('skip', true)}
-              disabled={controlDisabled}
-              error={fetchError}
-              icon="step-forward"
-            />
-          </div>
-        </CSSTransitionLazy>
-        <progress
-          className={classNames(
-            'progressbar',
-            fetchError ? 'danger' : 'primary'
-          )}
-          value={progress}
-        >
-          {progress}
-        </progress>
-        <PlayerNotification
-          alterationsResponse={responseOfSendPlayerCommandsSafe}
-          playerErrors={playerErrors}
-        />
-      </div>
-    )
-  }
+            }}
+            disabled={controlDisabled}
+            error={fetchError}
+            icon={isPlaying ? (playerStatus.paused ? 'play' : 'pause') : 'stop'}
+          />
+          <ManageButton
+            responseOfManage={responseOfSendPlayerCommandsSafe.fast_forward}
+            onClick={() => {
+              dispatch(sendPlayerCommand('fast_forward'))
+            }}
+            disabled={controlDisabled}
+            error={fetchError}
+            icon="forward"
+          />
+          <ManageButton
+            responseOfManage={responseOfSendPlayerCommandsSafe.skip}
+            onClick={() => {
+              dispatch(sendPlayerCommand('skip', true))
+            }}
+            disabled={controlDisabled}
+            error={fetchError}
+            icon="step-forward"
+          />
+        </div>
+      </CSSTransitionLazy>
+      <progress
+        className={classNames('progressbar', fetchError ? 'danger' : 'primary')}
+        value={progress}
+      >
+        {progress}
+      </progress>
+      <PlayerNotification
+        alterationsResponse={responseOfSendPlayerCommandsSafe}
+        playerErrors={playerErrors}
+      />
+    </div>
+  )
 }
 
 const ServerLost = () => (
@@ -226,19 +175,3 @@ const ServerLost = () => (
     </div>
   </div>
 )
-
-const mapStateToProps = (state) => ({
-  user: state.authenticatedUser,
-  playerStatusState: state.playlist.playerStatus,
-  playerErrorsDigestState: state.playlist.digest.playerErrors,
-  responseOfSendPlayerCommands:
-    state.alterationsResponse.multiple.sendPlayerCommands,
-})
-
-Player = withNavigate(
-  connect(mapStateToProps, {
-    sendPlayerCommand,
-  })(Player)
-)
-
-export default Player
