b501925ee7502dcda901350c69ea7d15b776788c
Neraste <neraste.herr10@gmail.com>
2025-11-23 20:21:54 +0100

    Convert UsersList to functional component

--- src/components/settings/users/List.jsx
+++ src/components/settings/users/List.jsx
@@ -1,137 +1,123 @@
-import PropTypes from 'prop-types'
-import { Component } from 'react'
-import { connect } from 'react-redux'
+import { useCallback, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useSearchParams } from 'react-router'
 
 import { getUsers } from 'actions/users'
 import { FormBlock, InputField } from 'components/generics/Form'
 import ListingFetchWrapper from 'components/generics/listing/FetchWrapper'
 import Navigator from 'components/generics/Navigator'
-import SettingsUserEntry from 'components/settings/users/Entry'
+import UserEntry from 'components/settings/users/Entry'
 import { IsUserManager } from 'permissions/Users'
-import { listUsersStatePropType } from 'reducers/users'
-import { withSearchParams } from 'thirdpartyExtensions/ReactRouterDom'
+import { Status } from 'reducers/alterationsResponse'
 
-class UsersList extends Component {
-  static propTypes = {
-    getUsers: PropTypes.func.isRequired,
-    listUsersState: listUsersStatePropType.isRequired,
-    searchParams: PropTypes.object.isRequired,
-  }
+export default function UsersList() {
+  const listUsersState = useSelector((state) => state.settings.users.list)
+  const { status: listUsersStatus } = listUsersState
 
-  componentDidMount() {
-    this.refreshEntries()
-  }
+  const [searchParams, _] = useSearchParams()
+  const { page } = Object.fromEntries(searchParams.entries())
 
-  componentDidUpdate(prevProps) {
-    const { searchParams } = this.props
-    const { searchParams: prevSearchParams } = prevProps
+  const dispatch = useDispatch()
 
-    // refresh if moved to a different page
-    const page = searchParams.get('page')
-    const prevPage = prevSearchParams.get('page')
-    if (page !== prevPage) {
-      this.refreshEntries()
-    }
-  }
+  const refreshEntries = useCallback(
+    () => {
+      dispatch(getUsers(page))
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [page]
+  )
 
-  refreshEntries = () => {
-    this.props.getUsers(this.props.searchParams.get('page') || 1)
-  }
+  useEffect(
+    () => {
+      // refresh the users immediately and if the page changes
+      if (listUsersStatus !== Status.pending) {
+        refreshEntries()
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [page]
+  )
 
-  render() {
-    const { users, pagination } = this.props.listUsersState.data
+  const { users, pagination } = listUsersState.data
 
-    const userList = users.map((user) => (
-      <SettingsUserEntry key={user.id} user={user} />
-    ))
+  const userList = users.map((user) => <UserEntry key={user.id} user={user} />)
 
-    return (
-      <div id="users-list">
-        <ListingFetchWrapper status={this.props.listUsersState.status}>
-          <div className="listing-table-container">
-            <table className="listing users-list notifiable">
-              <thead>
-                <tr className="listing-header">
-                  <th className="notification-col"></th>
-                  <th className="username">User&shy;name</th>
-                  <IsUserManager>
-                    <th className="validated">Email check</th>
-                    <th className="validated">Manager check</th>
-                  </IsUserManager>
-                  <th className="superuser">Super&shy;user</th>
-                  <th className="permission">Users rights</th>
-                  <th className="permission">Library rights</th>
-                  <th className="permission">Playlist rights</th>
-                  <th className="controls-col"></th>
-                </tr>
-              </thead>
-              <tbody>{userList}</tbody>
-            </table>
-          </div>
-        </ListingFetchWrapper>
-        <Navigator
-          count={users.length}
-          pagination={pagination}
-          names={{
-            singular: 'user',
-            plural: 'users',
-          }}
-        />
-        <IsUserManager>
-          <div className="create-user flow">
-            <FormBlock
-              title="Create user"
-              submitText="Create"
-              alterationName="createUser"
-              action="users/"
-              successMessage="User sucessfully created!"
-              onSuccess={this.refreshEntries}
-            >
-              <InputField id="username" label="Username" required />
-              <InputField
-                id="email"
-                label="Email"
-                required
-                validate={(value) => {
-                  if (!/\S+@\S+\.\S+/.test(value.toLowerCase())) {
-                    return ['This should be a valid email address.']
-                  }
-                }}
-              />
-              <InputField
-                id="password"
-                type="password"
-                label="Password"
-                required
-              />
-              <InputField
-                id="confirm_password"
-                type="password"
-                label="Confirm password"
-                required
-                validate={(value, values) => {
-                  if (values.password !== value) {
-                    return ['This field should match password field.']
-                  }
-                }}
-                ignore
-              />
-            </FormBlock>
-          </div>
-        </IsUserManager>
-      </div>
-    )
-  }
+  return (
+    <div id="users-list">
+      <ListingFetchWrapper status={listUsersStatus}>
+        <div className="listing-table-container">
+          <table className="listing users-list notifiable">
+            <thead>
+              <tr className="listing-header">
+                <th className="notification-col"></th>
+                <th className="username">User&shy;name</th>
+                <IsUserManager>
+                  <th className="validated">Email check</th>
+                  <th className="validated">Manager check</th>
+                </IsUserManager>
+                <th className="superuser">Super&shy;user</th>
+                <th className="permission">Users rights</th>
+                <th className="permission">Library rights</th>
+                <th className="permission">Playlist rights</th>
+                <th className="controls-col"></th>
+              </tr>
+            </thead>
+            <tbody>{userList}</tbody>
+          </table>
+        </div>
+      </ListingFetchWrapper>
+      <Navigator
+        count={users.length}
+        pagination={pagination}
+        names={{
+          singular: 'user',
+          plural: 'users',
+        }}
+      />
+      <IsUserManager>
+        <div className="create-user flow">
+          <FormBlock
+            title="Create user"
+            submitText="Create"
+            alterationName="createUser"
+            action="users/"
+            successMessage="User sucessfully created!"
+            onSuccess={() => {
+              refreshEntries()
+            }}
+          >
+            <InputField id="username" label="Username" required />
+            <InputField
+              id="email"
+              label="Email"
+              required
+              validate={(value) => {
+                if (!/\S+@\S+\.\S+/.test(value.toLowerCase())) {
+                  return ['This should be a valid email address.']
+                }
+              }}
+            />
+            <InputField
+              id="password"
+              type="password"
+              label="Password"
+              required
+            />
+            <InputField
+              id="confirm_password"
+              type="password"
+              label="Confirm password"
+              required
+              validate={(value, values) => {
+                if (values.password !== value) {
+                  return ['This field should match password field.']
+                }
+              }}
+              ignore
+            />
+          </FormBlock>
+        </div>
+      </IsUserManager>
+    </div>
+  )
 }
-
-const mapStateToProps = (state) => ({
-  listUsersState: state.settings.users.list,
-})
-
-UsersList = withSearchParams(
-  connect(mapStateToProps, {
-    getUsers,
-  })(UsersList)
-)
-
-export default UsersList
