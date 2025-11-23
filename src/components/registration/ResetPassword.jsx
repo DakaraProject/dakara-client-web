748030f88066741a80460885e013991c2afaebad
Neraste <neraste.herr10@gmail.com>
2025-11-23 03:38:05 +0100

    Convert ResetPassword to functional component

--- src/components/registration/ResetPassword.jsx
+++ src/components/registration/ResetPassword.jsx
@@ -1,86 +1,71 @@
-import PropTypes from 'prop-types'
-import queryString from 'query-string'
-import { Component } from 'react'
-import { connect } from 'react-redux'
-import { Navigate, NavLink } from 'react-router'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate, NavLink, useSearchParams } from 'react-router'
 
 import { FormBlock, InputField } from 'components/generics/Form'
-import { withLocation } from 'thirdpartyExtensions/ReactRouterDom'
 
-class ResetPassword extends Component {
-  static propTypes = {
-    isLoggedIn: PropTypes.bool.isRequired,
-    location: PropTypes.object.isRequired,
+export default function ResetPassword() {
+  const isLoggedIn = useSelector((state) => !!state.token)
+
+  const [emailSent, setEmailSent] = useState(false)
+
+  const [searchParams, _] = useSearchParams()
+
+  const { user_id, timestamp, signature } = Object.fromEntries(
+    searchParams.entries()
+  )
+
+  if (isLoggedIn) {
+    return <Navigate to="/" replace />
   }
 
-  state = {
-    emailSent: false,
-  }
+  const resetPasswordForm = (
+    <FormBlock
+      action="accounts/reset-password/"
+      submitText="Reset password"
+      alterationName="resetPassword"
+      successMessage={false}
+      pendingMessage={false}
+      formatValues={(values) => ({
+        user_id,
+        timestamp,
+        signature,
+        ...values,
+      })}
+      onSuccess={() => {
+        setEmailSent(true)
+      }}
+    >
+      <InputField id="password" type="password" label="Password" required />
+      <InputField
+        id="password_confirm"
+        type="password"
+        label="Confirm password"
+        required
+        validate={(value, values) => {
+          if (values.password !== value) {
+            return ['This field should match password field.']
+          }
+        }}
+      />
+    </FormBlock>
+  )
 
-  render() {
-    const { isLoggedIn } = this.props
-    const queryObj = queryString.parse(this.props.location.search)
+  const emailSentMessage = (
+    <p>
+      Password reset successful, you can now{' '}
+      <NavLink to="/login">login</NavLink>.
+    </p>
+  )
 
-    const { user_id, timestamp, signature } = queryObj
-
-    if (isLoggedIn) {
-      return <Navigate to="/" replace />
-    }
-
-    const resetPasswordForm = (
-      <FormBlock
-        action="accounts/reset-password/"
-        submitText="Reset password"
-        alterationName="resetPassword"
-        successMessage={false}
-        pendingMessage={false}
-        formatValues={(values) => ({
-          user_id,
-          timestamp,
-          signature,
-          ...values,
-        })}
-        onSuccess={() => this.setState({ emailSent: true })}
-      >
-        <InputField id="password" type="password" label="Password" required />
-        <InputField
-          id="password_confirm"
-          type="password"
-          label="Confirm password"
-          required
-          validate={(value, values) => {
-            if (values.password !== value) {
-              return ['This field should match password field.']
-            }
-          }}
-        />
-      </FormBlock>
-    )
-
-    const emailSentMessage = (
-      <p>
-        Password reset successful, you can now{' '}
-        <NavLink to="/login">login</NavLink>.
-      </p>
-    )
-
-    return (
-      <div id="reset-password" className="box neutral">
-        <div className="header primary">
-          <h2>Reset password</h2>
-        </div>
-        <div className="flow">
-          {this.state.emailSent ? emailSentMessage : resetPasswordForm}
-        </div>
+  return (
+    <div id="reset-password" className="box neutral">
+      <div className="header primary">
+        <h2>Reset password</h2>
       </div>
-    )
-  }
+      <div className="flow">
+        {emailSent ? emailSentMessage : resetPasswordForm}
+      </div>
+    </div>
+  )
 }
-
-const mapStateToProps = (state) => ({
-  isLoggedIn: !!state.token,
-})
-
-ResetPassword = withLocation(connect(mapStateToProps)(ResetPassword))
-
-export default ResetPassword
