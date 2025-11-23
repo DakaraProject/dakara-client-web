ca1ae2f2d3a38ed3ef24215419a9752c70e586d7
Neraste <neraste.herr10@gmail.com>
2025-11-23 20:01:14 +0100

    Convert UsersEdit to functional component

--- src/components/settings/users/Edit.jsx
+++ src/components/settings/users/Edit.jsx
@@ -1,6 +1,6 @@
-import PropTypes from 'prop-types'
-import { Component } from 'react'
-import { connect } from 'react-redux'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useLocation, useParams } from 'react-router'
 
 import { clearUser, getUser } from 'actions/users'
 import {
@@ -13,179 +13,159 @@ import Forbidden from 'components/navigation/Forbidden'
 import NotFound from 'components/navigation/NotFound'
 import { IsNotSelf, IsUserManager } from 'permissions/Users'
 import { Status } from 'reducers/alterationsResponse'
-import { editUsersStatePropType } from 'reducers/users'
-import { userPropType } from 'serverPropTypes/users'
-import { withLocation, withParams } from 'thirdpartyExtensions/ReactRouterDom'
 
-class UsersEdit extends Component {
-  static propTypes = {
-    authenticatedUser: userPropType.isRequired,
-    clearUser: PropTypes.func.isRequired,
-    editUsersState: editUsersStatePropType.isRequired,
-    getUser: PropTypes.func.isRequired,
-    location: PropTypes.object.isRequired,
-    params: PropTypes.shape({
-      userId: PropTypes.any.isRequired,
-    }).isRequired,
-    serverSettings: PropTypes.object,
+export default function UsersEdit() {
+  const editUsersState = useSelector((state) => state.settings.users.edit)
+  const authenticatedUser = useSelector((state) => state.authenticatedUser)
+  const serverSettings = useSelector((state) => state.internal.serverSettings)
+
+  const params = useParams()
+  const { userId } = params
+
+  const location = useLocation()
+
+  const dispatch = useDispatch()
+
+  useEffect(
+    () => {
+      // load user immediately
+      dispatch(getUser(userId))
+
+      return () => {
+        // unload user when component unmounts
+        dispatch(clearUser())
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  )
+
+  const { user } = editUsersState.data
+
+  // render nothing if the user is being fetched
+  if (editUsersState.status === Status.pending) {
+    return null
   }
 
-  componentDidMount() {
-    const userId = this.props.params.userId
-    this.props.getUser(userId)
+  // render an error page if the current user has no right to display the page
+  const fakeUser = { id: userId }
+  if (
+    !(
+      IsUserManager.hasPermission(authenticatedUser, fakeUser) &&
+      IsNotSelf.hasPermission(authenticatedUser, fakeUser)
+    )
+  ) {
+    return <Forbidden location={location} />
   }
 
-  componentWillUnmount() {
-    this.props.clearUser()
+  // render an error page if the requested user does not exist
+  if (!user) {
+    return <NotFound location={location} />
   }
 
-  render() {
-    const { location, authenticatedUser, serverSettings } = this.props
-    const { user } = this.props.editUsersState.data
-
-    // render nothing if the user is being fetched
-    if (this.props.editUsersState.status === Status.pending) {
-      return null
-    }
-
-    // render an error page if the current user has no right to display the page
-    const userId = this.props.params.userId
-    const fakeUser = { id: userId }
-    if (
-      !(
-        IsUserManager.hasPermission(authenticatedUser, fakeUser) &&
-        IsNotSelf.hasPermission(authenticatedUser, fakeUser)
-      )
-    ) {
-      return <Forbidden location={location} />
-    }
-
-    // render an error page if the requested user does not exist
-    if (!user) {
-      return <NotFound location={location} />
-    }
-
-    let passwordField
-    let passwordConfirmField
-    if (serverSettings && !serverSettings.email_enabled) {
-      passwordField = (
-        <InputField
-          id="password"
-          type="password"
-          label="Password"
-          ignoreIfEmpty
-        />
-      )
-      passwordConfirmField = (
-        <InputField
-          id="confirm_password"
-          type="password"
-          label="Confirm password"
-          validate={(value, values) => {
-            if (values.password !== value) {
-              return ['This field should match password field.']
-            }
-          }}
-          ignore
-        />
-      )
-    }
-
-    return (
-      <div id="users-edit" className="flow">
-        <FormBlock
-          title={`Edit user “${user.username}”`}
-          action={`users/${user.id}/`}
-          method="PATCH"
-          submitText="Edit"
-          alterationName="updateUser"
-          successMessage="User sucessfully updated!"
-          noClearOnSuccess
-        >
-          <InputField
-            id="username"
-            label="Username"
-            defaultValue={user.username}
-            disabled
-            ignore
-          />
-          <InputField
-            id="email"
-            label="E-mail"
-            defaultValue={user.email}
-            disabled
-            ignore
-          />
-          {passwordField}
-          {passwordConfirmField}
-          <CheckboxField
-            id="validated_by_email"
-            label="Validated by email"
-            defaultValue={user.validated_by_email}
-            disabled
-            ignore
-          />
-          <CheckboxField
-            id="validated_by_manager"
-            label="Validated by manager"
-            defaultValue={user.validated_by_manager}
-          />
-          <CheckboxField
-            id="is_superuser"
-            label="Superuser"
-            defaultValue={user.is_superuser}
-            disabled
-            ignore
-          />
-          <SelectField
-            id="users_permission_level"
-            label="Users rights"
-            defaultValue={user.users_permission_level}
-            options={[
-              { value: null, name: 'None' },
-              { value: 'u', name: 'User' },
-              { value: 'm', name: 'Manager' },
-            ]}
-          />
-          <SelectField
-            id="library_permission_level"
-            label="Library rights"
-            defaultValue={user.library_permission_level}
-            options={[
-              { value: null, name: 'None' },
-              { value: 'u', name: 'User' },
-              { value: 'm', name: 'Manager' },
-            ]}
-          />
-          <SelectField
-            id="playlist_permission_level"
-            label="Playlist rights"
-            defaultValue={user.playlist_permission_level}
-            options={[
-              { value: null, name: 'None' },
-              { value: 'u', name: 'User' },
-              { value: 'm', name: 'Manager' },
-            ]}
-          />
-        </FormBlock>
-      </div>
+  let passwordField
+  let passwordConfirmField
+  if (serverSettings && !serverSettings.email_enabled) {
+    passwordField = (
+      <InputField
+        id="password"
+        type="password"
+        label="Password"
+        ignoreIfEmpty
+      />
+    )
+    passwordConfirmField = (
+      <InputField
+        id="confirm_password"
+        type="password"
+        label="Confirm password"
+        validate={(value, values) => {
+          if (values.password !== value) {
+            return ['This field should match password field.']
+          }
+        }}
+        ignore
+      />
     )
   }
-}
 
-const mapStateToProps = (state) => ({
-  editUsersState: state.settings.users.edit,
-  authenticatedUser: state.authenticatedUser,
-  serverSettings: state.internal.serverSettings,
-})
-
-UsersEdit = withLocation(
-  withParams(
-    connect(mapStateToProps, {
-      getUser,
-      clearUser,
-    })(UsersEdit)
+  return (
+    <div id="users-edit" className="flow">
+      <FormBlock
+        title={`Edit user “${user.username}”`}
+        action={`users/${user.id}/`}
+        method="PATCH"
+        submitText="Edit"
+        alterationName="updateUser"
+        successMessage="User sucessfully updated!"
+        noClearOnSuccess
+      >
+        <InputField
+          id="username"
+          label="Username"
+          defaultValue={user.username}
+          disabled
+          ignore
+        />
+        <InputField
+          id="email"
+          label="E-mail"
+          defaultValue={user.email}
+          disabled
+          ignore
+        />
+        {passwordField}
+        {passwordConfirmField}
+        <CheckboxField
+          id="validated_by_email"
+          label="Validated by email"
+          defaultValue={user.validated_by_email}
+          disabled
+          ignore
+        />
+        <CheckboxField
+          id="validated_by_manager"
+          label="Validated by manager"
+          defaultValue={user.validated_by_manager}
+        />
+        <CheckboxField
+          id="is_superuser"
+          label="Superuser"
+          defaultValue={user.is_superuser}
+          disabled
+          ignore
+        />
+        <SelectField
+          id="users_permission_level"
+          label="Users rights"
+          defaultValue={user.users_permission_level}
+          options={[
+            { value: null, name: 'None' },
+            { value: 'u', name: 'User' },
+            { value: 'm', name: 'Manager' },
+          ]}
+        />
+        <SelectField
+          id="library_permission_level"
+          label="Library rights"
+          defaultValue={user.library_permission_level}
+          options={[
+            { value: null, name: 'None' },
+            { value: 'u', name: 'User' },
+            { value: 'm', name: 'Manager' },
+          ]}
+        />
+        <SelectField
+          id="playlist_permission_level"
+          label="Playlist rights"
+          defaultValue={user.playlist_permission_level}
+          options={[
+            { value: null, name: 'None' },
+            { value: 'u', name: 'User' },
+            { value: 'm', name: 'Manager' },
+          ]}
+        />
+      </FormBlock>
+    </div>
   )
-)
-
-export default UsersEdit
+}
