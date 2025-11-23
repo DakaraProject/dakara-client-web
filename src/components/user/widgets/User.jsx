2ea1ecff58cbbd3f9abc19f67dd1844ea83f5d87
Neraste <neraste.herr10@gmail.com>
2025-11-23 03:54:11 +0100

    Convert UserWidget to functional component

--- src/components/user/widgets/User.jsx
+++ src/components/user/widgets/User.jsx
@@ -1,38 +1,27 @@
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
-import { Component } from 'react'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 import { userPropType } from 'serverPropTypes/users'
 
-class UserWidget extends Component {
-  static propTypes = {
-    currentUser: userPropType.isRequired,
-    user: userPropType.isRequired,
-    truncatable: PropTypes.bool,
-  }
+export default function UserWidget({ user, truncatable }) {
+  const currentUser = useSelector((state) => state.authenticatedUser)
 
-  render() {
-    const { user, currentUser, truncatable } = this.props
-    const isCurrentUser = currentUser && currentUser.id === user.id
+  const isCurrentUser = currentUser && currentUser.id === user.id
 
-    return (
-      <div className={classNames('user-widget', { truncatable })}>
-        <span className="icon">
-          <i
-            className={isCurrentUser ? 'las la-user' : 'las la-user-friends'}
-          ></i>
-        </span>
-        <span className="name">{user.username}</span>
-      </div>
-    )
-  }
+  return (
+    <div className={classNames('user-widget', { truncatable })}>
+      <span className="icon">
+        <i
+          className={isCurrentUser ? 'las la-user' : 'las la-user-friends'}
+        ></i>
+      </span>
+      <span className="name">{user.username}</span>
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => ({
-  currentUser: state.authenticatedUser,
-})
-
-UserWidget = connect(mapStateToProps)(UserWidget)
-
-export default UserWidget
+UserWidget.propTypes = {
+  user: userPropType.isRequired,
+  truncatable: PropTypes.bool,
+}
