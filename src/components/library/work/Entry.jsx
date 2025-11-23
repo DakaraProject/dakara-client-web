bca255bb10bc520a33e8858623fff56c9991b59d
Neraste <neraste.herr10@gmail.com>
2025-11-23 15:52:52 +0100

    Convert WorkEntry to functional component

--- src/components/library/work/Entry.jsx
+++ src/components/library/work/Entry.jsx
@@ -1,54 +1,41 @@
 import PropTypes from 'prop-types'
 import queryString from 'query-string'
-import { Component } from 'react'
+import { useNavigate } from 'react-router'
 
 import { ListingEntry } from 'components/generics/listing/Entry'
 import WorkWidget from 'components/library/widgets/Work'
 import { workPropType } from 'serverPropTypes/library'
-import { withNavigate } from 'thirdpartyExtensions/ReactRouterDom'
 
-class WorkEntry extends Component {
-  static propTypes = {
-    work: workPropType.isRequired,
-    workType: PropTypes.string.isRequired,
-    navigate: PropTypes.func.isRequired,
-    query: PropTypes.object,
-  }
+export default function WorkEntry({ work, workType, query }) {
+  const navigate = useNavigate()
 
-  /**
-   * Search songs associated with the work
-   */
-  handleSearch = () => {
-    const query = `${this.props.workType}:""${this.props.work.title}""`
-    this.props.navigate({
-      pathname: '/library/song',
-      search: queryString.stringify({ query }),
-    })
-  }
+  const controls = (
+    <button
+      className="control square primary"
+      onClick={() => {
+        navigate({
+          pathname: '/library/song',
+          search: queryString.stringify({
+            query: `${workType}:""${work.title}""`,
+          }),
+        })
+      }}
+    >
+      <span className="icon">
+        <i className="las la-search"></i>
+      </span>
+    </button>
+  )
 
-  render() {
-    const { work, query } = this.props
-    const controls = (
-      <button
-        className="control square primary"
-        onClick={() => {
-          this.handleSearch()
-        }}
-      >
-        <span className="icon">
-          <i className="las la-search"></i>
-        </span>
-      </button>
-    )
-
-    return (
-      <ListingEntry id={work.id} controls={controls}>
-        <WorkWidget work={work} query={query} noIcon truncatable />
-      </ListingEntry>
-    )
-  }
+  return (
+    <ListingEntry id={work.id} controls={controls}>
+      <WorkWidget work={work} query={query} noIcon truncatable />
+    </ListingEntry>
+  )
 }
 
-WorkEntry = withNavigate(WorkEntry)
-
-export default WorkEntry
+WorkEntry.propTypes = {
+  work: workPropType.isRequired,
+  workType: PropTypes.string.isRequired,
+  query: PropTypes.object,
+}
