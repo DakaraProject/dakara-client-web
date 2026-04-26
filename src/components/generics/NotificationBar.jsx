99522b6a4aac7ea370291594a478e75c9314e71f
Neraste <neraste.herr10@gmail.com>
2026-04-26 15:20:59 +0200

    Add react-transitioning

---
+++ src/components/generics/NotificationBar.jsx
@@ -0,0 +1,146 @@
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+
+import { useDefaultTransitionState } from 'hooks/transitions'
+import {
+  alterationResponsePropType,
+  Status,
+} from 'reducers/alterationsResponse'
+
+const types = {
+  [Status.pending]: 'success',
+  [Status.successful]: 'success',
+  [Status.failed]: 'danger',
+}
+
+export default function NotificationBar({
+  alterationResponse = {},
+  failedDuration = 5000,
+  failedMessage = 'Failure',
+  pendingMessage = 'Pending…',
+  successfulDuration = 3000,
+  successfulMessage = 'Success',
+  noDisplayOnMount = false,
+}) {
+  const [state, toggle] = useDefaultTransitionState()
+
+  const durations = useMemo(
+    () => ({
+      [Status.successful]: successfulDuration,
+      [Status.failed]: failedDuration,
+    }),
+    [successfulDuration, failedDuration]
+  )
+  const messages = useMemo(
+    () => ({
+      [Status.pending]: pendingMessage,
+      [Status.successful]: successfulMessage,
+      [Status.failed]: failedMessage,
+    }),
+    [pendingMessage, successfulMessage, failedMessage]
+  )
+
+  const { status, date } = alterationResponse
+
+  const getMessage = useCallback(
+    (alterationResponse) => {
+      const { status, fields, message } = alterationResponse
+
+      // specific case if the alteration response contains field errors
+      if (fields && Object.keys(fields).length) {
+        return 'There are field errors'
+      }
+
+      // specific case if the alteration contains a message
+      if (message) {
+        return message
+      }
+
+      // default to specified messages
+      return messages[status]
+    },
+    [messages]
+  )
+
+  useEffect(() => {
+    if (!status || !date) {
+      return
+    }
+
+    // display if status and date changed and have a valid value
+    toggle(true)
+
+    // request to hide success or failure message only after a certain time
+    let timeout
+    if (status !== Status.pending && durations[status]) {
+      timeout = setTimeout(() => {
+        toggle(false)
+      }, durations[status])
+    }
+
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout)
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [status, date])
+
+  const notificationMessage = getMessage(alterationResponse)
+
+  if (notificationMessage && state.isMounted) {
+    return (
+      <div className={`notification-bar notified ${state.status}`}>
+        <div className={`notification non-hoverizable ${types[status] || ''}`}>
+          <div className="message">{getMessage(alterationResponse)}</div>
+        </div>
+      </div>
+    )
+  }
+
+  return null
+}
+
+NotificationBar.propTypes = {
+  alterationResponse: alterationResponsePropType,
+  failedDuration: PropTypes.number,
+  failedMessage: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  pendingMessage: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  successfulDuration: PropTypes.number,
+  successfulMessage: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  noDisplayOnMount: PropTypes.bool,
+}
+
+/**
+ * Helper to create a notifiable area in a table
+ *
+ * Must be used on the cell of the first column.
+ */
+export function NotifiableForTable({ className, children }) {
+  const elementRef = useRef()
+
+  const [parentTableElement, setParentTableElement] = useState(null)
+
+  useEffect(() => {
+    // find parent table DOM node
+    const element = elementRef.current
+    setParentTableElement(element.closest('table'))
+  }, [])
+
+  // get the width of the element with the width of the closest table
+  const width = parentTableElement?.clientWidth
+
+  return (
+    <div className="notifiable-for-table" ref={elementRef}>
+      <div className={classNames('notifiable', className)} style={{ width }}>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+NotifiableForTable.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+}
