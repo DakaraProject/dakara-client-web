5861ca32ba687f68b2db82b8ac2d31db1b15e0c6
Neraste <neraste.herr10@gmail.com>
2026-08-01 20:54:47 +0200

    Adjust header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ export default function Footer() {
   const projectHomepage = import.meta.env.DAKARA_PROJECT_HOMEPAGE
 
   return (
-    <footer id="footer" className="box">
+    <footer id="footer" className="box neutral">
       <div className="flow">
         <h2>
           Dakara client <span className="version">{version}</span>
         </h2>
-        <p className="contact text">
+        <p className="contact">
           Visit the <a href={projectHomepage}>project page</a>
           <br />
           Report a <a href={bugtracker}>bug</a>
